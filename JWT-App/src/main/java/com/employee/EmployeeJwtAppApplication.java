81d01f0a6f104b699015079f7701f6b27ab2fc84
kamit9171 <55205232+kamit9171@users.noreply.github.com>
2023-09-22 11:11:11 +0530

    Update EmployeeJwtAppApplication.java

--- JWT-App/src/main/java/com/employee/EmployeeJwtAppApplication.java
+++ JWT-App/src/main/java/com/employee/EmployeeJwtAppApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.Employee")
+@EnableJpaRepositories("com.employee")
 public class EmployeeJwtAppApplication {
 
 	public static void main(String[] args) {
